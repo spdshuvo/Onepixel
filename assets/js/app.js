157fecacb93709026946ef4ab2ccd78325255ca4
spdshuvo <shuvodev47@gmail.com>
2021-09-07 01:19:31 +0600

    html skill scetion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,23 @@
 $(function(){
     'use strict';
+    //banner slick slider plugin
     $(".banner_slider").slick()
+    //filterizer plugin
+    $('.filterizer-container').filterizr()
+    //magnific-popup
+    $(".popup-youtube").magnificPopup({
+        disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false
+    })
+    $('#bar1').barfiller({
+        barColor:'#000000',
+    });
+    $('#bar2').barfiller();
+    $('#bar3').barfiller();
+    $('#bar4').barfiller();
+    $('#bar5').barfiller();
 })
