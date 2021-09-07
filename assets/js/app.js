0f8bece3fec16636346adf348698e0d19e63c193
spdshuvo <shuvodev47@gmail.com>
2021-09-08 01:00:39 +0600

    upto brand html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,61 @@
-$(function(){
-    'use strict';
-    //banner slick slider plugin
-    $(".banner_slider").slick()
-    //filterizer plugin
-    $('.filterizer-container').filterizr()
-    //magnific-popup
-    $(".popup-youtube").magnificPopup({
-        disableOn: 700,
-		type: 'iframe',
-		mainClass: 'mfp-fade',
-		removalDelay: 160,
-		preloader: false,
-		fixedContentPos: false
-    })
-    $('#bar1').barfiller({
-        barColor:'#000000',
-    });
-    $('#bar2').barfiller();
-    $('#bar3').barfiller();
-    $('#bar4').barfiller();
-    $('#bar5').barfiller();
-})
+$(function () {
+            'use strict';
+            //banner slick slider plugin
+            $(".banner_slider").slick()
+            //filterizer plugin
+            $('.filterizer-container').filterizr()
+            //magnific-popup
+            $(".popup-youtube").magnificPopup({
+                disableOn: 700,
+                type: 'iframe',
+                mainClass: 'mfp-fade',
+                removalDelay: 160,
+                preloader: false,
+                fixedContentPos: false
+            })
+            $('#bar1').barfiller({
+                barColor: '#000000',
+            });
+            $('#bar2').barfiller();
+            $('#bar3').barfiller();
+            $('#bar4').barfiller();
+            $('#bar5').barfiller();
+            $('.counter').counterUp({
+                delay: 10,
+                time: 1000,
+            })
+            //Owl carousel
+            $('.testimonial_item_container').owlCarousel({
+                margin:0,
+                loop:true,
+                autoplay:true,
+                autoplayTimeout:4000,
+                nav:false,
+                smartSpeed:800,
+                navText:['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>'],
+                URLhashListener:true,
+                startPosition: 'URLHash',
+                responsive:{
+                    0:{
+                        items:1
+                    },
+                    450:{
+                        items:1
+                    },
+                    768:{
+                        items:1
+                    },
+                    1000:{
+                        items:1
+                    }
+                }
+            })
+            //brand slick active
+            $(".brand_name").slick({
+                slidesToShow:5,
+                slidesToScroll:1,
+                autoplay:true,
+                autoplaySpeed:3000,
+                arrows:false,
+            })
+            })
